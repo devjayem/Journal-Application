54475367bf19b53674fb3ca26adcfb0923a49fa6
devjayem <jmeugenio824@gmail.com>
2021-04-13 01:14:29 +0800

    create user and update homepage

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,11 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "bootstrap/dist/css/bootstrap"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+require('bootstrap')
+require('./hello_react')
---
+++ test/controllers/pages_controller_test.rb
@@ -0,0 +1,13 @@
+require "test_helper"
+
+class PagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get home" do
+    get pages_home_url
+    assert_response :success
+  end
+
+  test "should get todo_items" do
+    get pages_todo_items_url
+    assert_response :success
+  end
+end
